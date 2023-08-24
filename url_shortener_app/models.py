40d594d2a0158ad34d31f86223ec33d1c812cda4
nsardra@gmail.com <vibhans@4>
2023-08-24 23:47:36 +0530

    database configuration and models created

--- url_shortener_app/models.py
+++ url_shortener_app/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
+
+class RegisteredUsers(AbstractUser):
+    groups = models.ManyToManyField(Group, related_name='url_shortener_users')
+    user_permissions = models.ManyToManyField(Permission, related_name='url_shortener_users')
+    email = models.EmailField(unique=True)
+
+class Url(models.Model):
+    user_id = models.ForeignKey(RegisteredUsers, on_delete=models.CASCADE)
+    url = models.URLField(max_length=200)
+    url_id = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
